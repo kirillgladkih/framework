7636ed6d200e50b3fea4be601b5f5a13e25bc6ee
Kirill Gladkih <kirillgladkih2019@gmail.com>
2022-05-12 11:34:12 +0500

    add app services

--- core/Resolver/Resolver.php
+++ core/Resolver/Resolver.php
@@ -3,13 +3,14 @@
 namespace Core\Resolver;
 
 use Core\Resolver\Exception\ResolveException;
+use Laminas\Diactoros\ServerRequest;
 
 class Resolver implements IResolver
 {
-    public function resolve(mixed $handler): callable
+    public function resolve(mixed $handler, ServerRequest $request): callable
     {
         if(is_array($handler))
-            return [new $handler[0], $handler[1]];
+            return [new $handler[0]($request), $handler[1]];
 
         throw new ResolveException("method {$handler[1]} not exitst in {$handler[0]} class");
     }
--- index.php
+++ index.php
@@ -1,49 +1,10 @@
 <?php
 
-use Core\Request\Exception\IRequestExceptiion;
-use Core\Resolver\Resolver;
-use Core\Response\Sender;
-use Core\Routing\Router\Router;
-use Laminas\Diactoros\Response\JsonResponse;
-use Laminas\Diactoros\ServerRequestFactory;
+use Core\Services\App;
 
 require_once __DIR__ . "/bootstrap.php";
 
 ini_set('display_errors', 1);
 ini_set("error_reporting", E_ALL);
-
-try {
-    $request = ServerRequestFactory::fromGlobals();
-
-    $router = new Router();
-
-    $router->loadMap(include(__DIR__ . "/routes/api.php"));
-
-    $route = $router->match($request);
-
-    foreach ($route->getAttributeCollection()->all() as $key => $attribute)
-        $request = $request->withAttribute($key, $attribute);
-
-    $resolver = new Resolver();
-
-    foreach($route->getMiddlewareCollection()->all() as $item){
-
-        $middleware = new $item($request);
-
-        $handler = $middleware->handle($route->getHandler());
-
-    }
-
-    $response = $resolver->resolve($handler)($request);
-
-} catch (IRequestExceptiion $exception) {
-
-    $response = new JsonResponse(
-        ["errors" => $exception->getMessage()],
-        $exception->getCode()
-    );
-
-
-}
-
-Sender::send($response);
+$app = new App();
+$app->run();
