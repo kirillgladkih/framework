0c66fb2108034406e58655ca0fd1560c0a399bc6
Kirill Gladkih <kirillgladkih20192gmail.com>
2022-05-20 14:37:44 +0500

    restore controller

---
+++ app/Controllers/Controller.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Validation\TestValidator;
+use Core\Controllers\BaseController;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * КАЖДЫЙ МЕТОД КОНТРОЛЛЕРА ДОЛЖЕН ПРИНИМАТЬ ServerRequestInterface $request
+ * И ВОЗВРАЩАТЬ ResponseInterface
+ * НАПРИМЕР $this->jsonResponse(), РЕАЛИЗАЦИЯ ДАННОГО МЕТОДА НАХОДИТСЯ В
+ * /framework/core/Controllers/BaseController.php
+ */
+
+class Controller extends BaseController
+{
+    /**
+     * Undocumented function
+     *
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function index(ServerRequestInterface $request): ResponseInterface
+    {
+        return $this->jsonResponse(["id" => $request->getAttribute("id")]);
+    }
+
+    public function store(ServerRequestInterface $request): ResponseInterface
+    {
+        /**
+         * ПРИМЕР ВАЛИДАТОРА СМОТРЕТЬ app/Validation/TestValidator.php
+         */
+
+        $validator = new TestValidator($request);
+
+        $validator->make();
+
+        return $this->jsonResponse($validator->errors());
+    }
+}
